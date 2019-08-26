35266c568ad7620e4bed5750151225c5a5ed2f7b
Oliver Uhrig <oliver_uhrig@manulife.com>
2019-08-26 11:08:27 -0400

    small change

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import app from './server';
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Your app is running on port ${port}`); // eslint-disable-line
+  console.log(`Your app now is running on port ${port}`); // eslint-disable-line
 });
